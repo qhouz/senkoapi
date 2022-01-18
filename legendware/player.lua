40a1c57747c2e9bf430b3bc87a2575180a65c37d
qhouz <73645601+qhouz@users.noreply.github.com>
2022-01-18 10:18:59 +0700

    Create player.lua

---
+++ legendware/player.lua
@@ -0,0 +1,64 @@
+-- ! [Player] Simplification
+function player:is_alive()
+  if not self or not self:is_player() then
+    return end
+
+  return self:get_health() > 0.0
+end
+
+function player:get_eye_position()
+  if not self or not self:is_player() then
+    return end
+  
+  local origin = self:get_origin()
+  local eye_offset = vector.new(
+    self:get_prop_float('CBasePlayer', 'm_vecViewOffset[0]'),
+    self:get_prop_float('CBasePlayer', 'm_vecViewOffset[1]'),
+    self:get_prop_float('CBasePlayer', 'm_vecViewOffset[2]')
+  )
+  return origin + eye_offset
+end
+
+function player:get_weapon()
+  if not self or not self:is_player() then
+    return end
+  
+  local weapon = entitylist.get_weapon_by_player(self)
+  if not weapon or not weapon:is_weapon() then
+    return end
+
+  return weapon
+end
+
+function player:get_player_info()
+  if not self or not self:is_player() then
+    return end
+  
+  local info = engine.get_player_info(self:get_index())
+  if not info then
+    return end
+    
+  return info
+end
+
+function player:get_name()
+  if not self or not self:is_player() then
+    return '' end
+  
+  local info = self:get_player_info()
+  if not info then
+    return '' end
+    
+  return info.name
+end
+
+function player:is_teammate()
+  local player = entitylist.get_local_player()
+  if not player then
+    return false end
+  
+  if not self or not self:is_player() then
+    return false end
+    
+  return (self:get_team() == player:get_team())
+end
